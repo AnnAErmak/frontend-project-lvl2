4ba3e2b7cbe7e4cdda2de982c05bc7637e74fdf5
Anna Ermak <ermak-anna86@yandex.ru>
2022-03-08 19:05:00 +0300

    step 4 not test for linter.yml

--- src/genDiff.js
+++ src/genDiff.js
@@ -5,34 +5,34 @@ import _ from 'lodash';
 export default function genDiff(filepath1, filepath2) {
   const pathFile1 = path.resolve(filepath1);
   const pathFile2 = path.resolve(filepath2);
+
   const rawDataFile1 = readFileSync(pathFile1, 'utf8');
   const rawDataFile2 = readFileSync(pathFile2, 'utf8');
   const dataFile1 = JSON.parse(rawDataFile1);
   const dataFile2 = JSON.parse(rawDataFile2);
-  const arrF1 = Object.keys(dataFile1);
-  const arrF2 = Object.keys(dataFile2);
-  for (const item of arrF2) {
-    arrF1.push(item);
+  const keysFile1 = Object.keys(dataFile1);
+  const keysFile2 = Object.keys(dataFile2);
+  for (const item of keysFile2) {
+    keysFile1.push(item);
   }
-  const unicArr = _.uniq(arrF1);
-  const sortArr = _.sortBy(unicArr);
-  const resObj = {};
-  for (const key of sortArr) {
+  const unicKeys = _.uniq(keysFile1);
+  const sortKeys = _.sortBy(unicKeys);
+  let result = '{\n';
+  for (const key of sortKeys) {
     if (Object.hasOwn(dataFile1, key) && Object.hasOwn(dataFile2, key)) {
       if (dataFile1[key] !== dataFile2[key]) {
-        resObj[` - ${key}`] = dataFile1[key];
-        resObj[` + ${key}`] = dataFile2[key];
+        result += ` - ${key}: ${dataFile1[key]}\n + ${key}: ${dataFile2[key]}\n`;
       } else {
-        resObj[key] = dataFile1[key];
+        result += `   ${key}: ${dataFile1[key]}\n`;
       }
     }
     if (Object.hasOwn(dataFile1, key) && !(Object.hasOwn(dataFile2, key))) {
-      resObj[` - ${key}`] = dataFile1[key];
+      result += ` - ${key}: ${dataFile1[key]}\n`;
     }
     if (!(Object.hasOwn(dataFile1, key)) && Object.hasOwn(dataFile2, key)) {
-      resObj[` + ${key}`] = dataFile2[key];
+      result += ` + ${key}: ${dataFile2[key]}\n`;
     }
   }
-  console.log(resObj);
-  return resObj
+  result += '}';
+  return result;
 }
